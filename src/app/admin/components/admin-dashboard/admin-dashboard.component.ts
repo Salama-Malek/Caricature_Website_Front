90bab021b2152e1f680ad75b78df71074b2d8404
Salama Malek <salamahassanein@gmail.com>
2023-05-25 22:17:28 +0300

    fixed some errors in admin dashboard

--- src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  
+  ngOnInit(): void {
+    const sidebarToggleTop = document.getElementById('sidebarToggleTop');
 
+    if (sidebarToggleTop) {
+      sidebarToggleTop.addEventListener('click', () => {
+        const body = document.querySelector('body');
+        if (body) {
+          body.classList.toggle('sidebar-toggled');
+        }
+        const sidebar = document.querySelector('.sidebar');
+        if (sidebar) {
+          sidebar.classList.toggle('toggled');
+        }
+      });
+    }
+  }
 }
